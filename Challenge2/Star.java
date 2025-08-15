fa0ddd7647fc51936e3bc16bb384b93717c17166
BitSplice <salmanansari74070@gmail.com>
2025-08-15 15:50:22 +0530

    NEW CHALLENGE

---
+++ Challenge2/Star.java
@@ -0,0 +1,10 @@
+
+public class Star {
+
+    public static void main(String[] args) {
+        System.out.println("\n*\n* *\n* * *\n* * * *\n* * * * *");
+        System.out.println("\n\n* * * * *\n* * * *\n* * *\n* *\n*\n");
+        System.out.println("     *\n    * *\n   * * *\n  * * * *\n * * * * *\n");
+        System.out.println("\n        *\n      * *\n    * * *\n  * * * *\n* * * * *");
+    }
+}
