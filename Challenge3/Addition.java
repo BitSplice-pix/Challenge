ee28b7529d20424c3318c5cbfa0516634553dedc
BitSplice <salmanansari74070@gmail.com>
2025-08-15 15:52:36 +0530

    NEW CHALLENGE

--- Challenge3/Addition.java
+++ Challenge3/Addition.java
@@ -1,7 +1,8 @@
 
 import java.util.Scanner;
 
-public class Addition{
+public class Addition {
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("WELCOME TO BASIC CALCULATOR!");
